586f9cca2fe359e810fa5e04e9cd9e5a6c6671dd
Librazy <im@librazy.org>
2017-10-28 14:51:58 +0800

    fix error that makes me optional

--- src/main/java/cat/nyaa/clipectator/CommandHandler.java
+++ src/main/java/cat/nyaa/clipectator/CommandHandler.java
@@ -124,6 +124,6 @@ public class CommandHandler extends CommandReceiver {
     @SubCommand(value = "version")
     public void version(CommandSender sender, Arguments args) {
         String ver = plugin.getDescription().getVersion();
-        msg(sender, "manual.license", ver, plugin.getDescription().getAuthors().stream().reduce((a, b) -> a + ", " +b));
+        msg(sender, "manual.license", ver, plugin.getDescription().getAuthors().stream().reduce((a, b) -> a + ", " +b).orElse("NyaaCat Developers"));
     }
 }
